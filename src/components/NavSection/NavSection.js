649b09ac53bda7871d24a34afe31f40a57277928
lmjcbs <liam@lmjcbs.com>
2020-10-26 10:42:29 +0000

    add NavSection scroll logic

--- src/components/NavSection/NavSection.js
+++ src/components/NavSection/NavSection.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-
 import { BREAKPOINTS } from "../../constants";
 import Banner from "images/banner.svg";
 import Header from "images/header.svg";
 import PostcodeSearch from "../PostcodeSearch/index"
 import LocationFilter from "components/LocationFilter";
+
+const NavSection = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+    const visible = prevScrollPos > currentScrollPos;
+  
+    setPrevScrollPos(currentScrollPos);
+    setVisible(visible);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  })
+
+  return (
+    <NavSectionContainer>
+      <img className={!visible ? "header header--hidden" : "header"} src={Header} alt={"FREE SCHOOL MEALS"} />
+      <img className="banner" src={Banner} alt={"FREE SCHOOL MEALS"} />
+      <div className="filters">
+        <PostcodeSearch />
+        <LocationFilter />
+      </div>
+    </NavSectionContainer>
+  );
+};
 
 const NavSectionContainer = styled.div`
   width: 100%;
@@ -16,9 +43,17 @@ const NavSectionContainer = styled.div`
   }
   .header{
     display: block;
+    width: 100%;
+    padding: 10px;
+    top: 0;
+    transition: top 0.6s;
     @media screen and (min-width: ${BREAKPOINTS.md}) {
       display: none;
     }
+  }
+  .header--hidden{
+    top: -50px;
+    /* display: none; */
   }
   .banner {
     display: none;
@@ -35,19 +70,5 @@ const NavSectionContainer = styled.div`
     }
   }
 `;
-
-const NavSection = () => {
-
-  return (
-    <NavSectionContainer>
-      <img className="header" src={Header} alt={"FREE SCHOOL MEALS"} />
-      <img className="banner" src={Banner} alt={"FREE SCHOOL MEALS"} />
-      <div className="filters">
-        <PostcodeSearch />
-        <LocationFilter />
-      </div>
-    </NavSectionContainer>
-  );
-};
 
 export default NavSection;
